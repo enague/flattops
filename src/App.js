2c4f685d1f8a3724687d736fcd2212d01c6c2dca
eric <ennague@gmail.com>
2018-10-11 13:31:54 -0700

    implment sass for styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
 
-class App extends Component {
+import Landing from './Landing.js'
+import Footer from './Footer.js'
+
+class Home extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Flat Top Eats</h1>
+      <div>
+       <nav class="navbar navbar-light bg-dark">
+        <a class="navbar-brand" href="#">Flat Top Eats</a>
+        <p>TODO: add logo, links to social media, responsive nav bar</p>
+       </nav>
+        <div className="container">
+          <Landing />
+        </div>
+        <Footer />
       </div>
     );
   }
 }
 
-export default App;
+export default Home;
---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const Footer = () => (
+    <div className='container'>
+        <h1> Footer </h1>
+        <p>TODO: have columns for necessary info links</p>
+    </div>
+)
+
+export default Footer;
